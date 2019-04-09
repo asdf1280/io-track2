e6065a347c1c4e801771acf6ef5ff211be7dd036
dhkim0800 <dhkim0800@noreply.github.com>
2019-04-09 22:53:28 +0900

    df

--- gamejsv119.js
+++ gamejsv119.js
@@ -48461,7 +48461,7 @@
                 o = n.getDirection(this.object.position.z, this.object.position.x, i, t);
             this.target = {
                 xD: r,
-                yD: o + window.recentShots * 3,
+                yD: o,
                 x: t + s.camChaseDst * Math.sin(o) * Math.cos(r),
                 y: e - s.camChaseDst * Math.sin(r),
                 z: i + s.camChaseDst * Math.cos(o) * Math.cos(r)
